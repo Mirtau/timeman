185f3297875729c117f28084cd57715e1a947e50
Mirtau <tjj72@comcast.net>
2019-06-18 18:26:02 -0600

    start inline styling

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
-
+import PropTypes from 'prop-types'
 
 class TodoItem extends Component {
   render() {
     return (
-      <p>{ this.props.todo.title }</p>
+      <div style = { { backgroundColor: '#f4f4f4' } } >
+        <p>{ this.props.todo.title }</p>
+      </div>
     );
   }
 }
+TodoItem.propTypes = {
+  todo: PropTypes.object.isRequired
+};
 
 export default TodoItem;
